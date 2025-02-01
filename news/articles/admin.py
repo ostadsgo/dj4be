bce4c83be41afc0d9aba5d3eaaf170474fbecd4c
Saeed Gholami <saeed.ghollami@gmail.com>
2025-02-02 00:06:06 +0330

    First commit.

---
+++ news/articles/admin.py
@@ -0,0 +1,16 @@
+from xml.etree.ElementTree import Comment
+from django.contrib import admin
+
+from .models import Article, Comment
+
+
+class CommentInline(admin.StackedInline):
+    model = Comment
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [CommentInline]
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
---
+++ news/articles/models.py
@@ -0,0 +1,32 @@
+from pyexpat import model
+from django.db import models
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("article_detail", kwargs={"pk": self.pk})
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(
+        Article, related_name="comments", on_delete=models.CASCADE
+    )
+    comment = models.TextField()
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.comment
+
+    def get_absolute_url(self):
+        return reverse("article_list")
